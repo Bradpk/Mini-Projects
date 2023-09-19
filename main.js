f7722ee48416297c7b771d812a09cfd8541d04c1
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-09-19 15:55:52 -0400

    g

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+//---------------------------------------------------------------------------------------------------------------------------------------
+// A temperature converter 
+const kelvin = 293
+let celsius = kelvin - 273 
 
+let newton = celsius * (33/100)
+newton = Math.floor(newton);
+
+let fahrenheit = celsius * (9/5) + 32
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`)
+
+console.log(`The temperature is ${newton} degrees Newtons`)
+//---------------------------------------------------------------------------------------------------------------------------------------
