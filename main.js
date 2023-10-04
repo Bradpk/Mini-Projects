374d938762c5920aec0d44f2771c5cffafa853e7
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-10-04 12:54:33 -0400

    g

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //---------------------------------------------------------------------------------------------------------------------------------------
 // A temperature converter 
 
-
 const fahrenheitText = document.getElementById('fahrenheit')
 const newtonText = document.getElementById('newton')
+const dogText = document.getElementById('dogYears')
 
 const kelvin = 293
 let celsius = kelvin - 273 
@@ -21,3 +21,16 @@ console.log(`The temperature is ${newton} degrees Newtons`)
 newtonText.innerHTML = `The temperature is ${newton} degrees Newtons`
 //---------------------------------------------------------------------------------------------------------------------------------------
 // Your age in dog Years
+
+let myAge = 30
+let earlyYears = 2 
+earlyYears *= 10.5
+let laterYears = myAge - 2
+laterYears *= 4
+let myAgeInDogYears = earlyYears + laterYears
+let myName = 'Brad'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+dogText.innerHTML = `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+
+//---------------------------------------------------------------------------------------------------------------------------------------
