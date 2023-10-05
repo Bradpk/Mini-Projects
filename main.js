a7d75017f4331f0fbc596e94d55b5a969ebfb98f
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-10-05 12:54:23 -0400

    g

--- main.js
+++ main.js
@@ -4,6 +4,11 @@
 const fahrenheitText = document.getElementById('fahrenheit')
 const newtonText = document.getElementById('newton')
 const dogText = document.getElementById('dogYears')
+const questionText = document.getElementById('question')
+const askEightBall = document.getElementById('askEightBall')
+const eightBallAnswer = document.getElementById('eightBallAnswer')
+
+
 
 const kelvin = 293
 let celsius = kelvin - 273 
@@ -35,3 +40,41 @@ dogText.innerHTML = `My name is ${myName}. I am ${myAge} years old in human year
 //---------------------------------------------------------------------------------------------------------------------------------------
 // Magic Eight Ball
 
+let userQuestion = 'Will the world explode soon?'
+questionText.innerHTML = userQuestion
+
+askEightBall.addEventListener('click', function(){
+let randomNumber = Math.floor(Math.random() * 8);
+let eightball = ''
+
+switch (randomNumber){
+  case 0:
+  eightball = 'It is certain'
+  break;
+  case 1:
+ eightball = 'It is decidedly so'
+  break;
+  case 2:
+eightball =  'Reply hazy try again'
+  break;
+ case 3:
+eightball = 'Cannot predict now'
+  break;
+ case 4:
+eightball = 'Do not count on it'
+  break;
+   case 5:
+eightball = 'My sources say no'
+  break;
+   case 6:
+eightball = 'Outlook not so good'
+  break;
+   case 7:
+eightball = 'Signs point to yes'
+  break;
+}
+
+eightBallAnswer.innerHTML = eightball
+})
+//---------------------------------------------------------------------------------------------------------------------------------------
+// 
